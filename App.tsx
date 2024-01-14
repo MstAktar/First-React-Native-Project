c3f89194437cfbf891c83ce0d1a6aaf93c262e4e
Mst Aktar <t2akms00@students.oamk.fi>
2024-01-14 18:07:12 +0200

    lesson3

--- App.tsx
+++ App.tsx
@@ -4,26 +4,62 @@ import Greeter from './Lessons/lesson2/Greeter';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={ { fontSize: 25, color:'white', backgroundColor: 'black' } }>Hello React Native World! I am Airen!</Text>
-      <Text style={ styles.textDemo }>You can not write text without Text elements.</Text>
-      <Text style= {{...styles.textDemo, ...styles.someOfOtherStyle}}>Combine two existing Object</Text>
-      <HelloWorld />
-      <HelloWorld />
-      <Greeter name="Airen"/>
-      <Greeter name="Talha"/>
-    </View>
+    //lesson2
+    // <View style={styles.container}>
+    //   <Text style={ { fontSize: 25, color:'white', backgroundColor: 'black' } }>Hello React Native World! I am Airen!</Text>
+    //   <Text style={ styles.textDemo }>You can not write text without Text elements.</Text>
+    //   <Text style= {{...styles.textDemo, ...styles.someOfOtherStyle}}>Combine two existing Object</Text>
+    //   <HelloWorld />
+    //   <HelloWorld />
+    //   <Greeter name="Airen"/>
+    //   <Greeter name="Talha"/>
+    // </View>
 
+    //lesson3
+    <View style={styles.container}>
+        <View style={{width: 80, height: 80, backgroundColor:'red'}}></View>
+        <View style={{width: 80, height: 80, backgroundColor:'green'}}></View>
+        <View style={{width: 80, height: 80, backgroundColor:'blue'}}></View>
+        <View style={{width: 80, height: 80, backgroundColor:'pink'}}></View>
+    </View>
     
   );
 }
 
+//lesson2
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#A66',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+
+//   },
+//   textDemo: {
+//     fontSize: 30,
+//     color: 'green',
+//     backgroundColor: 'white'
+//   },
+//   someOfOtherStyle: {
+//     backgroundColor: 'pink'
+//   }
+// });
+
+//lesson3
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#A66',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    alignItems: 'flex-start',
+    // alignItems: 'baseline',
+    // alignItems: 'flex-end',
+    // justifyContent: 'space-between',
+    justifyContent: 'space-evenly',
+    
+    width: '100%',
+    padding: 50,
 
   },
   textDemo: {
