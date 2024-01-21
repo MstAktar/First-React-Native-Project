0bf5e31118ad5aad19e72f1c33c3cec4586006ac
Mst Aktar <t2akms00@students.oamk.fi>
2024-01-21 16:54:05 +0200

    Column issue solved

--- App.tsx
+++ App.tsx
@@ -25,25 +25,23 @@ export default function App() {
       );}*/
 
     //Exercise-01:
-    <View style={styles.container}>
-        <View style={ styles.firstRow }>
-          <View style={{ ...styles.item, backgroundColor: '#22F', flex: 1 }}></View>
-          <View style={{ ...styles.item, backgroundColor: '#22A', flex: 2 }}></View>
-        </View>
-        <View style={styles.secondRow}>
-
-        </View>
-        <View style={styles.thirdRow}>
-            <View style={{...styles.item, backgroundColor: '#090', flex: 1 }}></View>
-            <View style={{...styles.item, backgroundColor: '#0B0', flex: 1 }}></View>
-            <View style={{...styles.item, backgroundColor: '#090', flex: 1 }}></View>
-        </View>
-        <View style={styles.forthRow}>
-            <View style={{backgroundColor: 'yellow', flexGrow: 1}}></View>
-            <View style={{backgroundColor: 'pink', flexGrow: 1}}></View>
-        </View>
-    </View> 
-  );
+<View style={ styles.container }>
+      <View style={ styles.firstRow }>
+        <View style={{ ...styles.item, backgroundColor: '#22F', flex: 1}}></View>
+        <View style={ {...styles.item, backgroundColor: '#22A', flex: 2}}></View>
+      </View>
+      <View style={styles.secondRow}></View>
+      <View style={styles.thirdRow}>
+        <View style={{...styles.item, backgroundColor: '#090', flex: 1}}></View>
+        <View style={{...styles.item, backgroundColor: '#0B0', flex: 1}}></View>
+        <View style={{...styles.item, backgroundColor: '#090', flex: 1}}></View>
+      </View>
+      <View style={styles.fourthRow}>
+        <View style={{backgroundColor: 'yellow', flexGrow: 1}}></View>
+        <View style={{backgroundColor: 'pink', flexGrow: 1}}></View>
+      </View>
+    </View>
+  )
 }
 
   //Images
@@ -107,12 +105,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#000',
-    alignItems: 'center',
+    backgroundColor: '#000',    
     justifyContent: 'flex-start',
     width: '100%',
-    height: '100%',
-    paddingTop: 50,
+    height: '100%',    
   },
   firstRow: {
     flex: 1,
@@ -121,22 +117,23 @@ const styles = StyleSheet.create({
   secondRow: {
     flex: 2,
     backgroundColor: '#0D0',
-    width:'100%',
+    width: '100%'
   },
   thirdRow: {
-    flex: 1,
     flexDirection: 'row',
+    flex: 1
   },
-  forthRow: {
-    flex: 1,
-    flexDirection: 'row',
+  fourthRow: {
+    flex: 1, 
+    flexDirection: 'column',
     justifyContent: 'flex-start',
-    alignItems: 'stretch',
+    alignItems: 'stretch'
   },
   item: {
-    height: '100%',
+    height: '100%'
   }
-});
+
+})
 
 //Images
 /*const styles = StyleSheet.create({
